5228cb6acafeb8ff667938160551c97784e01179
proxidev <joeledmond95@gmail.com>
2023-05-16 10:39:26 +0100

    add notification websocket and insert data on dom in page feed

--- apps/consumers.py
+++ apps/consumers.py
@@ -14,7 +14,10 @@ class NotificationConsumer(AsyncWebsocketConsumer):
         self.group_name = 'notification'
         await self.accept()
         await self.channel_layer.group_add(self.group_name, self.channel_name)
-        await self.send(text_data=json.dumps({"message": "Hello proxi"}))
+        await self.send(text_data=json.dumps(
+            {"message": "Hello proxi",
+             "name": "greeting"}
+            ))
         
 
     async def disconnect(self, close_code):
@@ -23,21 +26,33 @@ class NotificationConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         print(text_data)
-        message = "hello"
+        text_data_json = json.loads(text_data)
         event = {
             "type": "send_notif",
-            "message": "hello"
+             
         }
+        if text_data_json['type'] == "notif_feed":
+            data_json = await self.get_notif_from_db(type='notif_feed')
+            event["message"] = data_json
+            event["name"] = "notif_feed"
+            
+        if text_data_json['type'] == "notif_list":
+            data_json = await self.get_notif_from_db()
+            event["message"] = data_json
+            event["name"] = "notif_list"
+
+        
         await self.channel_layer.group_send(self.group_name, event)
-        # await self.send(text_data=json.dumps({"message": message}))
+        return await super().receive(text_data)
 
     async def send_notif(self, event):
-        data = await self.get_notif_from_db()
-        await self.send(text_data=data)
+        await self.send(text_data=json.dumps(event))
       
     @database_sync_to_async    
-    def get_notif_from_db(self):
+    def get_notif_from_db(self, type=None):
         notif_list = Notification.objects.all()
+        if type == "notif_feed":
+            notif_list = notif_list[:10]
         serializer = NotificationSerialiser(notif_list, many=True)
         return json.dumps(serializer.data)
         
--- apps/serializers.py
+++ apps/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, RelatedField
 
 from apps.models import Notification
 
@@ -10,14 +10,22 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields ='__all__'
+        
+
+class TargetSerializer(RelatedField):
+    
+    def to_representation(self, value):
+        return value.content
+    
 
 class NotificationSerialiser(ModelSerializer):
-    
+    target = TargetSerializer(read_only=True)
     class Meta:
         model = Notification
         fields = ['id',
                   'read',
                   'user',
+                  'target',
                   'created',
                   'action']
         
