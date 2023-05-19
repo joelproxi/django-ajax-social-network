3a56925d348ff1c437794878c1e99def65deaf26
proxidev <joeledmond95@gmail.com>
2023-05-19 20:46:01 +0100

    refactoring code

--- apps/models.py
+++ apps/models.py
@@ -22,9 +22,12 @@ class Post(CreationModificationMixin):
     content = models.TextField(null=True, blank=True)
     # slug = models.SlugField(max_length=500, unique=True)
     # image = models.ImageField(upload_to='images/post')
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, 
+                              on_delete=models.CASCADE)
 
-    users_like = models.ManyToManyField(User, related_name='user_like_post', through='LikePost')
+    users_like = models.ManyToManyField(User, 
+                                        related_name='user_like_post', 
+                                        through='LikePost')
     
     class Meta(CreationModificationMixin.Meta):
         pass
@@ -45,9 +48,13 @@ class Media(models.Model):
     
     
 class Comment(CreationModificationMixin):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_comment')
+    post = models.ForeignKey(Post, 
+                            on_delete=models.CASCADE, 
+                            related_name='post_comment')
     content = models.CharField(max_length=500)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posted')
+    owner = models.ForeignKey(User, 
+                            on_delete=models.CASCADE, 
+                            related_name='posted')
     # created_at = models.DateTimeField(auto_now_add=True)
     # updated_at = models.DateTimeField(auto_now=True)
     users_like = models.ManyToManyField(User, related_name='user_like_comment', through='LikeComment')
@@ -60,8 +67,12 @@ class Comment(CreationModificationMixin):
         
 
 class LikePost(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_like')
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_user')
+    post = models.ForeignKey(Post, 
+                            on_delete=models.CASCADE, 
+                            related_name='post_like')
+    owner = models.ForeignKey(User, 
+                            on_delete=models.CASCADE, 
+                            related_name='post_user')
     created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
@@ -69,8 +80,12 @@ class LikePost(models.Model):
 
 
 class LikeComment(models.Model):
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='comment_like')
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment_user')
+    comment = models.ForeignKey(Comment, 
+                                on_delete=models.CASCADE, 
+                                related_name='comment_like')
+    owner = models.ForeignKey(User, 
+                                on_delete=models.CASCADE, 
+                                related_name='comment_user')
     created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
@@ -78,16 +93,24 @@ class LikeComment(models.Model):
     
            
 class Follow(models.Model):
-    user_from = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_set')
-    user_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_to')
+    user_from = models.ForeignKey(User, 
+                                  on_delete=models.CASCADE, 
+                                  related_name='user_set')
+    user_to = models.ForeignKey(User, 
+                                on_delete=models.CASCADE, 
+                                related_name='user_to')
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return "%s started follow %s" % (self.user_from.get_username, self.user_to.get_username)
+        return "%s started follow %s" % (
+            self.user_from.get_username, 
+            self.user_to.get_username)
     
 
 User.add_to_class('follow_user',
-                  models.ManyToManyField('self', related_name='followed', symmetrical=False))
+                  models.ManyToManyField('self', 
+                                         related_name='followed', 
+                                         symmetrical=False))
 
 
 class Notification(models.Model):
